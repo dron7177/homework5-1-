9071127914ee6f9a1d784d0878ff2477ca8ce077
dron7177 <165674465+dron7177@users.noreply.github.com>
2024-04-10 15:07:21 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,10 @@
+my_list = ['apple', 'tomate', 'peach', 'pear', 'cucumber']
+print(my_list)
+print(my_list[0], my_list[4])
+print(my_list[2:4])
+my_list[2] = 'potato'
+print(my_list)
+my_dict = {'слон' : 'elephant', 'кролик' : 'rabbit', 'утка' : 'duck'}
+print(my_dict)
+my_dict['слон'] = 'wolf'
+print(my_dict)
